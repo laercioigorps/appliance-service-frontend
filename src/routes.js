ca48924a464479e006c8fe754508d4e9ed6c672a
Laercio Souza <laercioigorps@gmail.com>
2022-04-12 05:25:20 -0300

    added test page to routes

--- src/routes.js
+++ src/routes.js
@@ -49,6 +49,7 @@ const Modals = React.lazy(() => import('./views/notifications/modals/Modals'))
 const Toasts = React.lazy(() => import('./views/notifications/toasts/Toasts'))
 
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
+const Teste = React.lazy(() => import('./views/teste/teste'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
@@ -95,6 +96,7 @@ const routes = [
   { path: '/notifications/modals', name: 'Modals', element: Modals },
   { path: '/notifications/toasts', name: 'Toasts', element: Toasts },
   { path: '/widgets', name: 'Widgets', element: Widgets },
+  { path: '/teste', name: 'Teste', element: Teste },
 ]
 
 export default routes

373464eb4a3d69b5f16a88b9eabbaab07a4642c1
Laercio Souza <laercioigorps@gmail.com>
2022-07-16 18:05:48 -0300

    updated file to use customer history data in the first card

--- src/views/widgets/MainWidgetsDropdown.js
+++ src/views/widgets/MainWidgetsDropdown.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   CRow,
   CCol,
@@ -13,7 +13,43 @@ import { CChartBar, CChartLine } from '@coreui/react-chartjs'
 import CIcon from '@coreui/icons-react'
 import { cilArrowBottom, cilArrowTop, cilOptions } from '@coreui/icons'
 
+async function getCustomerHistory() {
+  return fetch('http://127.0.0.1:8000/profiles/customer-history', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Token ' + localStorage.getItem('token'),
+    },
+  }).then((data) => data.json())
+}
+
 const MainWidgetsDropdown = () => {
+  const [loaded, setLoaded] = useState(false)
+
+  const [customerLabels, setCustomerLabels] = useState(false)
+  const [customerData, setCustomerData] = useState(false)
+  const [customerTotal, setCustomerTotal] = useState(0)
+  const [customerDifference, setCustomerDifference] = useState(10)
+
+  useEffect(() => {
+    if (!loaded) {
+      getCustomerHistory().then((history) => {
+        console.log(history)
+        setCustomerData(history.data)
+        setCustomerLabels(history.labels)
+        setCustomerTotal(history.total)
+        calculateDiff(history.data)
+      })
+      setLoaded(true)
+    }
+  })
+
+  function calculateDiff(arr) {
+    let now = arr[arr.length - 1]
+    let last = arr[arr.length - 2]
+    setCustomerDifference(((now - last) / last) * 100)
+  }
+
   return (
     <CRow>
       <CCol sm={6} lg={3}>
@@ -22,13 +58,14 @@ const MainWidgetsDropdown = () => {
           color="primary"
           value={
             <>
-              26K{' '}
+              {customerTotal}{' '}
               <span className="fs-6 fw-normal">
-                (-12.4% <CIcon icon={cilArrowBottom} />)
+                ({customerDifference}%{' '}
+                <CIcon icon={customerDifference > 0 ? cilArrowTop : cilArrowBottom} />)
               </span>
             </>
           }
-          title="Users"
+          title="Customers"
           action={
             <CDropdown alignment="end">
               <CDropdownToggle color="transparent" caret={false} className="p-0">
@@ -47,14 +84,16 @@ const MainWidgetsDropdown = () => {
               className="mt-3 mx-3"
               style={{ height: '70px' }}
               data={{
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: customerLabels
+                  ? customerLabels
+                  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
                 datasets: [
                   {
                     label: 'My First dataset',
                     backgroundColor: 'transparent',
                     borderColor: 'rgba(255,255,255,.55)',
                     pointBackgroundColor: getStyle('--cui-primary'),
-                    data: [65, 59, 84, 84, 51, 55, 40],
+                    data: customerData ? customerData : [65, 59, 84, 84, 51, 55, 40],
                   },
                 ],
               }}
@@ -76,8 +115,8 @@ const MainWidgetsDropdown = () => {
                     },
                   },
                   y: {
-                    min: 30,
-                    max: 89,
+                    min: loaded && customerData ? Math.min(...customerData) - 5 : 40,
+                    max: loaded && customerData ? Math.max(...customerData) + 5 : 70,
                     display: false,
                     grid: {
                       display: false,

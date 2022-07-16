a8bb1bfc62bf7c7962e9bb3045181927c6867f61
Laercio Souza <laercioigorps@gmail.com>
2022-07-16 18:04:44 -0300

    updated dashboard to use differente widgetDropdown

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -53,6 +53,7 @@ import avatar6 from 'src/assets/images/avatars/6.jpg'
 
 import WidgetsBrand from '../widgets/WidgetsBrand'
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
+import MainWidgetsDropdown from '../widgets/MainWidgetsDropdown'
 
 const Dashboard = () => {
   const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
@@ -180,7 +181,7 @@ const Dashboard = () => {
 
   return (
     <>
-      <WidgetsDropdown />
+      <MainWidgetsDropdown />
       <CCard className="mb-4">
         <CCardBody>
           <CRow>

64daeef1faa5dc7d47d6657cf4121cd6b17e315f
Laercio Souza <laercioigorps@gmail.com>
2022-08-09 17:24:01 -0300

    added path to sample login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Suspense, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import useToken from './components/App/useToken'
 import './scss/style.scss'
+import SampleLogin from './views/sampleData/SampleLogin'
 
 const loading = (
   <div className="pt-3 text-center">
@@ -34,6 +35,7 @@ function App() {
           />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
+          <Route exact path="/sample-login" name="Sample Account Login" element={<SampleLogin />} />
           {!token ? (
             <Route exact path="*" name="Login Page" element={<Login setToken={setToken} />} />
           ) : (
